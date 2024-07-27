774a1d70968ac9a40fbb312338d985472a39d776
devandrews <developer.andrews@gmail.com>
2024-07-27 16:54:32 -0300

    fix: lint

--- src/external/pg/migrations/1722100025582_create_products_table.js
+++ src/external/pg/migrations/1722100025582_create_products_table.js
@@ -1,19 +1,19 @@
-exports.shorthands = undefined;
+exports.shorthands = undefined
 
 exports.up = (pgm) => {
-  pgm.createTable("products", {
-    id: "id",
-    name: { type: "varchar(100)", notNull: true },
-    category: { type: "varchar(100)", notNull: true },
-    price: { type: "decimal", notNull: true },
-    description: { type: "text", notNull: true },
-    images: { type: "text[]", notNull: true },
-    created_at: { type: "timestamp", default: pgm.func("current_timestamp") },
-    updated_at: { type: "timestamp", default: pgm.func("current_timestamp") },
-    deleted_at: { type: "timestamp" },
-  });
-};
+  pgm.createTable('products', {
+    id: 'id',
+    name: { type: 'varchar(100)', notNull: true },
+    category: { type: 'varchar(100)', notNull: true },
+    price: { type: 'decimal', notNull: true },
+    description: { type: 'text', notNull: true },
+    images: { type: 'text[]', notNull: true },
+    created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    updated_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    deleted_at: { type: 'timestamp' }
+  })
+}
 
 exports.down = (pgm) => {
-  pgm.dropTable("products");
-};
+  pgm.dropTable('products')
+}
--- src/external/pg/migrations/1722100025583_create_orders_table.js
+++ src/external/pg/migrations/1722100025583_create_orders_table.js
@@ -1,28 +1,28 @@
-exports.shorthands = undefined;
+exports.shorthands = undefined
 
 exports.up = (pgm) => {
-  pgm.createTable("orders", {
-    id: "id",
-    status: { type: "varchar(100)", notNull: true },
-    total: { type: "decimal", notNull: true },
-    created_at: { type: "timestamp", default: pgm.func("current_timestamp") },
-    updated_at: { type: "timestamp", default: pgm.func("current_timestamp") },
-    deleted_at: { type: "timestamp" },
-  });
+  pgm.createTable('orders', {
+    id: 'id',
+    status: { type: 'varchar(100)', notNull: true },
+    total: { type: 'decimal', notNull: true },
+    created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    updated_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    deleted_at: { type: 'timestamp' }
+  })
 
-  pgm.createTable("order_items", {
-    id: "id",
-    order_id: { type: "varchar(100)", notNull: true },
-    product_id: { type: "varchar(100)", notNull: true },
-    quantity: { type: "integer", notNull: true },
-    price: { type: "decimal", notNull: true },
-    created_at: { type: "timestamp", default: pgm.func("current_timestamp") },
-    updated_at: { type: "timestamp", default: pgm.func("current_timestamp") },
-    deleted_at: { type: "timestamp" },
-  });
-};
+  pgm.createTable('order_items', {
+    id: 'id',
+    order_id: { type: 'varchar(100)', notNull: true },
+    product_id: { type: 'varchar(100)', notNull: true },
+    quantity: { type: 'integer', notNull: true },
+    price: { type: 'decimal', notNull: true },
+    created_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    updated_at: { type: 'timestamp', default: pgm.func('current_timestamp') },
+    deleted_at: { type: 'timestamp' }
+  })
+}
 
 exports.down = (pgm) => {
-  pgm.dropTable("order_items");
-  pgm.dropTable("orders");
-};
+  pgm.dropTable('order_items')
+  pgm.dropTable('orders')
+}
